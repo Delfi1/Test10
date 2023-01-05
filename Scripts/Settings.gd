bf761685be1572e22bc84963e6b462d1358e7ea3
Delfi1 <fezdx@mail.ru>
2023-01-05 11:58:48 +0300

    Update 0.0.8

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -16,11 +16,21 @@ onready var Server_Ver = get_node("Info/Server_Version")
 
 var Maker = "res://Scenes/Maker.tscn"
 
-onready var ver = "0.0.7"
+onready var ver = "0.0.8"
 
 onready var Sver = null
 
+onready var Check = get_node("Info/Check")
+
+onready var UButton = get_node("Info/Update") 
+
+var dir = Directory.new()
+
+var path = OS.get_executable_path()
+
 func _ready():
+	$Info/HTTPRequest.connect("request_completed", self, "_on_download_completed")
+	$HTTPRequest.connect("request_completed", self, "_on_request_completed")
 	_on_Graphics_pressed()
 	Version.text = "Version: " + ver
 
@@ -55,3 +65,26 @@ func back():
 
 func _on_Create_pressed():
 	get_tree().change_scene(Maker)
+
+func _on_Check_pressed():
+	Check.disabled = true
+	$HTTPRequest.request("https://raw.githubusercontent.com/Delfi1/Test10/master/State/Version.txt")
+
+func _on_request_completed(result, response_code, headers, body):
+	Sver = body.get_string_from_utf8()
+	Server_Ver.text = "Server version: " + Sver
+	Check.disabled = false
+	if Sver != ver:
+		UButton.disabled = false
+
+func _on_Update_pressed():
+	dir.remove(path + "Test10.pck")
+	Check.disabled = true
+	UButton.disabled = true
+	$Info/HTTPRequest.set_download_file("Test10.pck")
+	$Info/HTTPRequest.request("https://github.com/Delfi1/Test10/blob/master/Export/Test10.pck?raw=true")
+
+func _on_download_completed(result, response_code, headers, body):
+	Check.disabled = false
+	UButton.disabled = false
+	get_tree().reload_current_scene()
