50e2be3e70f58b221c4077ed9d1df8ecd24703f1
Delfi1 <fezdx@mail.ru>
2023-01-05 16:09:10 +0300

    Update 0.1.1

--- Scenes/Maker.gd
+++ Scenes/Maker.gd
@@ -4,9 +4,7 @@ onready var camera = get_node("Camera2D")
 
 onready var Stats = get_node("HUD/Control/Stat")
 
-onready var Box = get_node("Box")
-
-const speed = 1
+onready var Box = load("res://Objects/Box.tscn")
 
 var Info = "FPS: " + String(Engine.get_frames_per_second())
 
@@ -17,22 +15,38 @@ var path = Vector2(stepify(Mouse_pos.x, 32), stepify(Mouse_pos.y, 32))
 var mid_x = 0
 var mid_y = 0
 
-var target = null
+var target
+
+enum Maker{IDLE, CREATE, LINK}
+
+var state = Maker.IDLE
 
 var Menu = "res://Scenes/Menu.tscn"
+
+var obj
 
 func _process(delta):
 	Info = "FPS: " + String(Engine.get_frames_per_second()) + "\n"
 
 	Mouse_pos = get_global_mouse_position()
 	
-	# Изменение позиции объекта
-	path = Vector2(stepify(Mouse_pos.x, 1), stepify(Mouse_pos.y, 1))
+	path = Vector2(stepify(Mouse_pos.x, 32), stepify(Mouse_pos.y, 32))
 	
-	Box.position = path
-	
-	Info += "x: " + String(Box.position.x) + "; y: " + String(Box.position.y)
-	
+	Info += "x: " + String(Mouse_pos.x) + "; y: " + String(Mouse_pos.y) + "\n"
+
+	match state:
+		Maker.IDLE:
+			pass
+		Maker.CREATE:
+			obj = Box.instance()
+			obj.mode = 1
+			obj.position = path
+			self.add_child(obj)
+			state = Maker.LINK
+		Maker.LINK:
+			obj.position = path
+			Info += "Obj.x: " + String(path.x) + "; Obj.y: " + String(path.y)
+
 	# Вывод текста на экран.
 	Stats.text = Info
 	
@@ -40,3 +54,9 @@ func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.scancode == KEY_ESCAPE:
 			get_tree().change_scene(Menu)
+	if Input.is_action_just_pressed("MouseLeft"):
+		if state == Maker.LINK:
+			state = Maker.IDLE
+			obj.position = Vector2(stepify(Mouse_pos.x, 32), stepify(Mouse_pos.y, 32))
+		else:
+			state = Maker.CREATE
